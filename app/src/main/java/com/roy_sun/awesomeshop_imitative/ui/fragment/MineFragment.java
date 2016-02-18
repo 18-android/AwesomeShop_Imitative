c9c6e2a6fec6b8be58e55aad6ef1ae5a9882a68c
Karen <hkl0138@foxmail.com>
2016-02-18 19:15:05 +0800

    登录，注册，我的中心三个布局

--- app/src/main/java/com/roy_sun/awesomeshop_imitative/ui/fragment/MineFragment.java
+++ app/src/main/java/com/roy_sun/awesomeshop_imitative/ui/fragment/MineFragment.java
@@ -3,41 +3,52 @@ package com.roy_sun.awesomeshop_imitative.ui.fragment;
 
 import com.roy_sun.awesomeshop_imitative.R;
 import com.roy_sun.awesomeshop_imitative.base.BaseFragment;
+import com.roy_sun.awesomeshop_imitative.utils.UIUtils;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MineFragment extends BaseFragment {
 
+    private TextView me_baobei;
 
-    public MineFragment() {
-        // Required empty public constructor
-    }
+    ImageButton btn_head;
+    private View byId;
+    private View byId1;
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
-    }
 
     @Override
     public View createView(LayoutInflater inflater,
                            ViewGroup container,
                            Bundle savedInstanceState) {
-        return null;
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        btn_head = (ImageButton) view.findViewById(R.id.btn_head);
+        btn_head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"111",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
 
     @Override
     public void init() {
 
+
     }
+
 
 }
