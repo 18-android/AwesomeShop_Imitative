5a77f055e3b81176207d4abcb1aa67b23438029d
Roy_sun <roy1993sun@gmail.com>
2016-02-19 15:25:28 +0800

    合并代码冲突, 完成了注册登录的跳转界面

--- app/src/main/java/com/roy_sun/awesomeshop_imitative/ui/fragment/MineFragment.java
+++ app/src/main/java/com/roy_sun/awesomeshop_imitative/ui/fragment/MineFragment.java
@@ -3,41 +3,46 @@ package com.roy_sun.awesomeshop_imitative.ui.fragment;
 
 import com.roy_sun.awesomeshop_imitative.R;
 import com.roy_sun.awesomeshop_imitative.base.BaseFragment;
+import com.roy_sun.awesomeshop_imitative.ui.activity.LoginActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MineFragment extends BaseFragment {
 
+    private ImageView like;
 
-    public MineFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
-    }
 
     @Override
     public View createView(LayoutInflater inflater,
                            ViewGroup container,
                            Bundle savedInstanceState) {
-        return null;
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        like = (ImageView) view.findViewById(R.id.btn_head);
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), LoginActivity.class));
+            }
+        });
+        return view;
     }
 
     @Override
     public void init() {
 
+
     }
+
 
 }
